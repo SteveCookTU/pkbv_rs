285ba011ef985f6e607cd9ec20810cea6c0a7091
Steve Cook <steviecook210@gmail.com>
2022-05-01 02:13:22 -0400

    Missing files

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+use pkbv_rs::my_app::MyApp;
+
+fn main() {
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(300.0, 300.0)),
+        resizable: false,
+        ..Default::default()
+    };
+    eframe::run_native(Box::new(MyApp::default()), options);
+}
